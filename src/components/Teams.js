23c0ef2ca7a6c26b2cfacb9e719b05ad423bfd31
hendraaagil <hendraaagil@gmail.com>
2020-12-28 09:00:10 +0700

    Add conditional rendering

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,20 +1,26 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import Team from './Team';
 
 const Teams = ({ teams }) => (
   <Flex gap={6} py={2} px={5} flexWrap="wrap" justifyContent="center">
-    {teams.teams?.map(team => (
-      <Team
-        key={team.idTeam}
-        name={team.strTeam}
-        badge={team.strTeamBadge}
-        alt={team.strAlternate}
-        stadiumName={team.strStadium}
-        stadium={team.strStadiumThumb}
-        desc={team.strDescriptionEN}
-        jersey={team.strTeamJersey}
-      />
-    ))}
+    {teams.teams ? (
+      teams.map(team => (
+        <Team
+          key={team.idTeam}
+          name={team.strTeam}
+          badge={team.strTeamBadge}
+          alt={team.strAlternate}
+          stadiumName={team.strStadium}
+          stadium={team.strStadiumThumb}
+          desc={team.strDescriptionEN}
+          jersey={team.strTeamJersey}
+        />
+      ))
+    ) : (
+      <Text fontSize="xl" fontWeight="bold">
+        Nothing here!
+      </Text>
+    )}
   </Flex>
 );
 
